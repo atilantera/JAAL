b4006c59180f07f1beb1b1e9caa0301c26bfb1e4
Artturi Tilanterä <artturi.tilantera@aalto.fi>
2021-06-23 17:42:13 +0300

    Schema: Metadata specification

--- spec/validate.sh
+++ spec/validate.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 echo "--- Valid schemas -----------------------------"
-ajv --spec=draft2020 -s jaal-1.1.json -d "test/valid/*.json"
+ajv test --spec=draft2020 -s jaal-1.1.json -d "test/valid/*.json" --valid
+#ajv --spec=draft2020 --errors=text -s jaal-1.1.json -d "test/valid/*.json"
 echo "--- Invalid schemas -----------------------------"
-ajv --spec=draft2020 -s jaal-1.1.json -d "test/invalid/*.json"
+ajv test --spec=draft2020 -s jaal-1.1.json -d "test/invalid/*.json" --invalid
+#ajv --spec=draft2020 --errors=text -s jaal-1.1.json -d "test/invalid/*.json"
