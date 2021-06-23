7cd7c8600901809deea9126b837f5850a879f248
Artturi Tilanterä <artturi.tilantera@aalto.fi>
2021-06-23 16:39:47 +0300

    Schema: main structure specified

--- spec/validate.sh
+++ spec/validate.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-ajv --spec=draft2020 -s jaal.json -d test/valid/*.json
+echo "--- Valid schemas -----------------------------"
+ajv --spec=draft2020 -s jaal-1.1.json -d "test/valid/*.json"
+echo "--- Invalid schemas -----------------------------"
+ajv --spec=draft2020 -s jaal-1.1.json -d "test/invalid/*.json"
