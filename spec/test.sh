40fce00559400d4c567d3fd12241700b2b07306f
Artturi Tilanterä <artturi.tilantera@aalto.fi>
2021-07-02 17:34:43 +0300

    Schema: Node with attributes

--- spec/test.sh
+++ spec/test.sh
@@ -4,20 +4,31 @@
 # ajv option -r: files *.json but not jaal.json are part of the schema
 # ajv option -d: directory for test data
 echo "--- Valid schemas -----------------------------"
+
 ajv test --spec=draft2020 -s schemas/attributeList.json \
   -d "test/valid/attributeList*.json" --valid
+
 ajv test --spec=draft2020 -s schemas/jaal.json -r schemas/metadata.json \
   -d "test/valid/jaal*.json" --valid
+
 ajv test --spec=draft2020 -s schemas/metadata.json -d "test/valid/metadata*" --valid
 
+ajv test --spec=draft2020 -s schemas/node.json -r schemas/attributeList.json \
+  -d "test/valid/node*.json" --valid
+
 echo "--- Invalid schemas -----------------------------"
+
 ajv test --spec=draft2020 -s schemas/attributeList.json \
--d "test/invalid/attributeList*.json" --invalid
+-d "test/invalid/attributeList*.json" --invalid --errors=text
 
 ajv test --spec=draft2020 -s schemas/jaal.json -r schemas/metadata.json \
 -d "test/invalid/jaal*.json" --invalid --errors=text
 
-ajv test --spec=draft2020 -s schemas/metadata.json -d "test/invalid/metadata*" --invalid
+ajv test --spec=draft2020 -s schemas/metadata.json -d "test/invalid/metadata*" --invalid --errors=text
+
+ajv test --spec=draft2020 -s schemas/node.json -r schemas/attributeList.json \
+  -d "test/invalid/node*.json" --invalid --errors=text
+
 
 # Bundle
 #ajv test --spec=draft2020 -s bundle/jaal.json -d "test/valid/*.json" --valid
