7c952dd4dbf140bdc16218f165c66983eec9619f
Artturi Tilanterä <artturi.tilantera@aalto.fi>
2022-05-16 16:48:01 +0300

    test.js: refactor, refine output, test time
    Several modifications were done in test.js.
    
    - refactoring: assert was removed in create_tests()
      -> no need for an if-else block for valid and invalid cases
    - more consistent output formatting in validate_schema()
    - total test running time printed
    
    Obsolete test.sh was removed.

--- spec/test.js
+++ spec/test.js
@@ -1,20 +1,19 @@
 const Ajv2020 = require("ajv/dist/2020");
-const { assert } = require("console");
 const fs = require("fs");
 var tests_passed = 0;
 var tests_failed = 0;
 
 /**
- * create_tests finds the test cases for the schema as located in 
- * /tests/valid and /tests/invalid. For each of the test cases, 
- * it calls validate_schema with the required parameters, and 
- * asserts that the expected outcome is met. 
- * @param schema_name takes the name of the root schema. 
- * @param validity takes "valid" or "invalid", 
+ * create_tests finds the test cases for the schema as located in
+ * /tests/valid and /tests/invalid. For each of the test cases,
+ * it calls validate_schema with the required parameters, and
+ * asserts that the expected outcome is met.
+ * @param schema_name takes the name of the root schema.
+ * @param validity takes "valid" or "invalid",
  * to check if a file is valid or invalid. This is also the folder
  * in which the test files are found.
- * @param dependencies takes a list of strings, on which the root 
- * schema is dependant. This is just the filenames, without extension. 
+ * @param dependencies takes a list of strings, on which the root
+ * schema is dependant. This is just the filenames, without extension.
  * If the schema is dependent on jaal.json, dependencies = ["jaal"].
  */
 
@@ -28,16 +27,12 @@ const create_tests = (schema_name, validity, dependencies) => {
         dependent_on = expand_dependencies(dependencies);
     }
     for (var i = 0; i < files.length; i++) {
-        if (validity === "valid") {
-            assert(validate_schema(schema, files[i], validity, dependent_on));
-        } else {
-            assert(!validate_schema(schema, files[i], validity, dependent_on));
-        }
+        validate_schema(schema, files[i], validity, dependent_on);
     }
 }
 
 /**
- * expand_dependencies takes a list of partial file names, 
+ * expand_dependencies takes a list of partial file names,
  * and expands this into the full path of the file:
  * schemas/{file_name}.json, where {file_name}
  * is the name of a partial file name.
@@ -56,17 +51,26 @@ const expand_dependencies = (dependencies = []) => {
 
 
 /**
- * validate_schema creates an ajv instance and validates the provided
- * test_file against the JAAL schema provided to determine whether 
- * it is valid JAAL. 
- * @param schema is the schema file. 
+ * validate_schema creates an ajv instance and validates the provided test_file
+ * against the JAAL schema provided to determine whether it is valid JAAL.
+ * @param schema is the schema file.
  * @param test_file is the file to be verified if it is valid JAAL.
  * @param validity takes the values "invalid" or "valid",
  * to check if a file is valid or invalid. This is also the folder
  * in which the test_file is found.
  * @param dependencies is a list of dependencies of the schema.
  * If empty, schema has no dependencies.
- * @returns a boolean of whether test_file is valid JAAL. 
+ *
+ * Increases global variables:
+ * - Increase tests_passed if:
+ *   - a test expected passing passed validation;
+ *   - a test expected failing fails validation with the expected error
+ *     location and error message.
+ * - Increase tests_failed if:
+ *   - a test expected passing fails validation;
+ *   - a test expected failing passes validation;
+ *   - a test expected failing fails validation, but either the error location
+ *     or message differs from the expected one.
  */
 
 const validate_schema = (schema, test_file, validity, dependencies = []) => {
@@ -77,53 +81,43 @@ const validate_schema = (schema, test_file, validity, dependencies = []) => {
     } else {
         validate = ajv.compile(schema);
     }
-        
+
     filename = "./test/" + validity + "/" + test_file;
     test_case = require(filename);
-    
-    const valid = validate(test_case);
 
-    if (valid) {
-        if (validity === "valid") {
-            tests_passed++;
-        } else {
+    const validation_passed = validate(test_case);
+
+    if (validation_passed) {
+        if (validity === "valid") { // Expect valid, validation passed
+          tests_passed++;
+        } else {                    // Expect valid, validation failed
             const file = "/" + validity + "/" + test_file;
-            console.log("Test", file, 
-                        "passed validation although it should not.");
+            console.log("Test", filename, "should not pass validation!");
             tests_failed++;
         }
     } else {
-        const file = "/" + validity + "/" + test_file;
-        if (validity === "valid") {
-            console.log("Test", file, 
-                        "failed validation although it should not.");
-            console.log(validate.errors); 
-            tests_failed++;   
-        } else {
-            /**
-             * Make sure that the correct error is produced. 
-             * The expected error is stored in the errorInstancePath
-             * and errorMessage field of the test files. 
-             */
+        if (validity === "valid") { // Expect invalid, validation passed
+            console.log("Test", file, "should not fail validation!.");
+            console.log(validate.errors);
+            tests_failed++;
+        } else {                    // Expect invalid, validation failed
+            /* Expected error location and message */
+            const expected_path = test_case.errorInstancePath;
+            const expected_msg  = test_case.errorMessage;
+            /* ajv-produced error location and message */
+            const found_path = validate.errors[0].instancePath;
+            const found_msg = validate.errors[0].message;
 
-            console.log("Test " + file +  ":");
-            console.log("\t", validate.errors[0].instancePath, 
-                              validate.errors[0].message);
-            const instancePath = validate.errors[0].instancePath;
-            const message = validate.errors[0].message;
-                        
-            if (test_case.errorInstancePath === instancePath && 
-                test_case.errorMessage === message) {
+            if (expected_path === found_path && expected_msg === found_msg) {
                 tests_passed++;
             } else {
-                console.log("Expected error: ", test_case.errorInstancePath, 
-                            test_case.errorMessage);
+                console.log("Test ", filename, ":");
+                console.log("  Expected error:", expected_path, expected_msg);
+                console.log("  Found error   :", found_path, found_msg)
                 tests_failed++;
             }
         }
-        
     }
-    return valid;
 };
 
 
@@ -133,7 +127,7 @@ const valid_edge_tests = () => {
 
 
 const valid_event_tests = () => {
-    create_tests("event", "valid"); 
+    create_tests("event", "valid");
 };
 
 
@@ -147,14 +141,14 @@ const valid_graph_tests = () => {
 
 
 const valid_initialState_tests = () => {
-    create_tests("initialState", "valid", 
+    create_tests("initialState", "valid",
                 ["edge", "keyvalue", "graph", "matrix", "node"]);
 }
 
 
 const valid_jaal_tests = () => {
-    create_tests("jaal", "valid", 
-                ["definitions", "edge", "event", "graph", "initialState", 
+    create_tests("jaal", "valid",
+                ["definitions", "edge", "event", "graph", "initialState",
                 "keyvalue", "matrix", "metadata", "node", "style"]);
 }
 
@@ -201,13 +195,13 @@ const invalid_graph_tests = () => {
 }
 
 const invalid_initialState_tests = () => {
-    create_tests("initialState", "invalid", 
+    create_tests("initialState", "invalid",
                 ["edge", "keyvalue", "graph", "matrix", "node"]);
 }
 
 const invalid_jaal_tests = () => {
-    create_tests("jaal", "invalid", 
-                ["definitions", "edge", "event", "graph", "initialState", 
+    create_tests("jaal", "invalid",
+                ["definitions", "edge", "event", "graph", "initialState",
                 "keyvalue", "matrix", "metadata", "node", "style"]);
 }
 
@@ -238,6 +232,9 @@ const invalid_style_tests = () => {
 
 
 function main() {
+    const test_time = "Tests run in";
+    console.time(test_time);
+
     valid_edge_tests();
     valid_event_tests();
     valid_definitions_tests();
@@ -261,8 +258,10 @@ function main() {
     invalid_metadata_tests();
     invalid_node_tests();
     invalid_style_tests();
-    
-    console.log(String(tests_passed), "tests passed,", 
+
+    console.timeEnd(test_time);
+
+    console.log(String(tests_passed), "tests passed,",
                 String(tests_failed), "tests failed");
 }
 
